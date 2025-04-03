043e7a0503fad4d0ccf5a5a46af9bcbe9240d16e
crwn-up <luidran1@gmail.com>
2025-04-03 14:31:24 -0600

    Update Ejercico 3.c

--- Ejercico 3.c
+++ Ejercico 3.c
@@ -3,3 +3,37 @@
   10 do while
   Escriba un programa en C que muestre en pantalla los números enteros del 1 al 100
   Escriba un programa qu pida al usuario 
+#include <stdio.h>
+
+int main() {
+    int numero;
+    int contador = 0;
+    int suma = 0;
+
+    printf("Introduce un número (0 para finalizar): ");
+    scanf("%d", &numero);
+
+    while (numero != 0) {
+        printf("Has introducido el número %d\n", numero);
+        contador++;
+        suma += numero;
+
+        printf("Introduce un número (0 para finalizar): ");
+        scanf("%d", &numero);
+    }
+
+    printf("Finalizando: Se ha introducido el número 0\n");
+    printf("Cantidad de números introducidos: %d\n", contador);
+    printf("Suma total de los números introducidos: %d\n", suma);
+
+    return 0;
+}
+
+#include <stdio.h>
+
+int main() {
+    for (int i = 1; i <= 100; i++) {
+        printf("%d\n", i);
+    }
+    return 0;
+}
