15f34af6e01f39044c4413b2daf5040854888ba7
crwn-up <luidran1@gmail.com>
2025-04-03 14:10:30 -0600

    Update Ejercicio1.c

--- Ejercicio1.c
+++ Ejercicio1.c
@@ -27,3 +27,45 @@ if (ultimo_par!=-1){
     }
     return 0;
     }
+#include <stdio.h>
+
+int main() {
+    int n;
+    int count_par = 0, count_impar = 0;
+    int ultimo_par = -1, ultimo_impar = -1;
+    int num = 1; // Inicializamos num en 1 para recorrer desde 1 hasta n
+
+    // Solicitar al usuario la cantidad de números a evaluar
+    printf("Introduzca la cantidad de números: ");
+    scanf("%d", &n);
+
+    // Evaluar números desde 1 hasta n
+    while (num <= n) {
+        if (num % 2 == 0) {
+            count_par++;
+            ultimo_par = num;
+        } else {
+            count_impar++;
+            ultimo_impar = num;
+        }
+        num++;
+    }
+
+    // Mostrar resultados
+    printf("\nCantidad de números pares: %d\n", count_par);
+    printf("Cantidad de números impares: %d\n", count_impar);
+
+    if (ultimo_par != -1) {
+        printf("Último número par: %d\n", ultimo_par);
+    } else {
+        printf("No se encontraron números pares.\n");
+    }
+
+    if (ultimo_impar != -1) {
+        printf("Último número impar: %d\n", ultimo_impar);
+    } else {
+        printf("No se encontraron números impares.\n");
+    }
+
+    return 0;
+}
