e0c4aa7cdf04de04009060bc559d919dee437fb6
crwn-up <luidran1@gmail.com>
2025-04-03 14:34:11 -0600

    Update Ejercico 3.c

--- Ejercico 3.c
+++ Ejercico 3.c
@@ -1,8 +1,6 @@
 1 for
   6 while
   10 do while
-  Escriba un programa en C que muestre en pantalla los números enteros del 1 al 100
-  Escriba un programa qu pida al usuario 
 #include <stdio.h>
 
 int main() {
