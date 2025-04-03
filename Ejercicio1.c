6287b81daebe38e73abc13c119abb820b52729d9
crwn-up <luidran1@gmail.com>
2025-04-03 14:08:29 -0600

    Create Ejercicio1.c

---
+++ Ejercicio1.c
@@ -0,0 +1,29 @@
+//Luis Crown
+//Ejercicio 1 Práctica 7
+#include stdio.h
+int main () {
+    int n;
+    int count_par=0, count_impar=0;
+    int ultimo_par=0, int ultimo_impar=0;
+    printf("Introduzca la cantidad de números");
+    scanf("%d", &n);
+    while (num=<n) {
+        if (num %2 == 0)
+        count_par++;
+        ultimo_par= num;
+    } else {
+        count_impar++;
+        ultimo_impar = num;
+    }
+    num++;
+}
+printf("\nCantidad de numeros pares: %d\n", countpar);
+printf("Cantidad de números impares:%d\n", countimpar);
+if (ultimo_par!=-1){
+    printf("Ultimo numero par: d%\n", ultimo_par);
+    }
+    if (ultimo_impar != -1) {
+        printf("ultimo numero impar: d%\n", ultimo_impar);
+    }
+    return 0;
+    }
